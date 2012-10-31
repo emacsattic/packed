aac0fcb542792e041f38b4a91cb9004806bb91aa
Jonas Bernoulli <jonas@bernoul.li>
2012-10-31 21:05:03 +0100

    improve library detection from git repositories
    * add packed-git-main-library
    * packed-git-library-p: add new REPOSITORY argument to front
    * packed-git-libraries-1: cleanup arguments and in doc-string say it's
      for internal use only

--- packed-git.el
+++ packed-git.el
@@ -80,19 +80,19 @@
       (after-insert-file-set-coding (- (point-max) (point-min)))
       coding)))
 
-(defun packed-git-library-p (commit file &optional package)
-  "Return non-nil if FILE is an Emacs source library and part of package.
+(defun packed-git-library-p (repository commit file &optional package)
+  "Return non-nil if FILE is an Emacs source library and part of PACKAGE.
 Actually return the feature provided by FILE.  For anything else
 including bundled libraries return nil.
 
-COMMIT has to be an existing commit in the current repository (as
-determined using `default-directory') and FILE has to exist in
-that commit.
+COMMIT has to be an existing commit in REPOSITORY and FILE has to
+exist in that commit.
 
 See function `packed-library-p' for more information."
   (and (packed-library-name-p file package)
-       (packed-with-blob commit file
-         (packed-library-feature file))))
+       (let ((default-directory (or repository default-directory)))
+         (packed-with-blob commit file
+           (packed-library-feature file)))))
 
 (defun packed-git-libraries (repository commit &optional package)
   "Return a list of libraries that are part of PACKAGE located in REPOSITORY.
@@ -102,18 +102,11 @@ existing commit in that repository.
 
 See function `packed-libraries' for more information."
   (let ((default-directory repository))
-    (packed-git-libraries-1
-     commit nil (or package (packed-filename repository)) t)))
+    (packed-git-libraries-1 (or package (packed-filename repository))
+                            commit nil)))
 
-(defun packed-git-libraries-1 (commit directory package &optional top-level)
-  "Return a list of Emacs lisp files in the package directory DIRECTORY.
-DIRECTORY is assumed to contain the libraries belonging to a
-single package.
-
-COMMIT has to be an existing commit in the current repository
-and FILE has to exist in that commit.
-
-See function `packed-libraries-1' for more information."
+(defun packed-git-libraries-1 (package commit tree)
+  "For internal use only."
   (let* ((regexp "^[0-9]\\{6\\} \\([^ ]+\\) [a-z0-9]\\{40\\}\t\\(.+\\)$")
          (objects
           (mapcar (lambda (line)
@@ -121,23 +114,50 @@ See function `packed-libraries-1' for more information."
                     (list (match-string 2 line)
                           (intern (match-string 1 line))))
                   (magit-git-lines "ls-tree" "--full-tree"
-                                   (concat commit ":" directory))))
+                                   (concat commit ":" tree))))
          (searchp (not (assoc ".nosearch" objects)))
          files)
-    (when (or top-level searchp)
+    (when (or (null tree) searchp)
       (dolist (object objects)
         (destructuring-bind (file type) object
-          (setq file (concat (and directory (file-name-as-directory directory))
-                             file))
+          (when tree
+            (setq file (concat (file-name-as-directory tree) file)))
           (ecase type
             (blob (and searchp
-                       (packed-git-library-p commit file package)
+                       (packed-git-library-p nil commit file package)
                        (push file files)))
             (tree (unless (packed-ignore-directory-p file package)
                     (setq files (nconc files (packed-git-libraries-1
-                                              commit file package)))))
+                                              package commit file)))))
             (commit))))
       (sort files 'string<))))
+
+(defun packed-git-main-library
+  (repository commit &optional package noerror nosingle)
+  "Return the main library of the PACKAGE in REPOSITORY.
+PACKAGE is the name of the package and REPOSITORY is the root
+directory of it's git repository.
+
+Return the library whose basename matches the package name.  If
+that fails append \"-mode\" to the package name, respectively
+remove that substring, and try again.
+
+The library must provide the correct feature; that is the feature
+which matches the filename (and possibly parts of the path leading
+to it).
+
+Unless optional NOSINGLE is non-nil and if there is only a single
+Emacs lisp file return that even if it doesn't match the package
+name.
+
+If the main library cannot be found raise an error or if optional
+NOERROR is non-nil return nil."
+  (unless package
+    (setq package (packed-filename repository)))
+  (let ((default-directory repository))
+    (packed-main-library-1 package
+                           (packed-git-libraries-1 package commit nil)
+                           noerror nosingle)))
 
 (provide 'packed-git)
 ;; Local Variables:

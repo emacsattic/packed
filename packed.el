394efe60bc7ba4f45f355d820b26bfe5494b01f8
Jonas Bernoulli <jonas@bernoul.li>
2020-10-24 11:54:37 +0200

    packed-update-autoloads: Silence Emacs 28 byte-compiler

--- packed.el
+++ packed.el
@@ -1,6 +1,6 @@
 ;;; packed.el --- package manager agnostic Emacs Lisp package utilities  -*- lexical-binding: t -*-
 
-;; Copyright (C) 2012-2018  Jonas Bernoulli
+;; Copyright (C) 2012-2020  Jonas Bernoulli
 
 ;; Author: Jonas Bernoulli <jonas@bernoul.li>
 ;; Homepage: https://github.com/emacscollective/packed
@@ -407,7 +407,10 @@ nil if not found."
 
 (defun packed-update-autoloads (dest path)
   (packed-with-loaddefs dest
-    (update-directory-autoloads path)))
+    (cond ((fboundp 'make-directory-autoloads)   ; >= 28
+           (make-directory-autoloads path generated-autoload-file))
+          ((fboundp 'update-directory-autoloads) ; <= 27
+           (update-directory-autoloads path)))))
 
 (defun packed-remove-autoloads (dest path)
   (packed-with-loaddefs dest

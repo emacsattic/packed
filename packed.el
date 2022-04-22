2d4ed03032b3ddc008182f48bd3b81dbda0cbefc
Jonas Bernoulli <jonas@bernoul.li>
2022-04-22 18:26:45 +0200

    Refresh library header

--- packed.el
+++ packed.el
@@ -1,28 +1,27 @@
-;;; packed.el --- package manager agnostic Emacs Lisp package utilities  -*- lexical-binding: t -*-
+;;; packed.el --- Package manager agnostic Emacs Lisp package utilities  -*- lexical-binding:t -*-
 
-;; Copyright (C) 2012-2022  Jonas Bernoulli
+;; Copyright (C) 2012-2022 Jonas Bernoulli
 
 ;; Author: Jonas Bernoulli <jonas@bernoul.li>
 ;; Homepage: https://github.com/emacscollective/packed
-;; Keywords: compile, convenience, lisp, package, library
+;; Keywords: lisp
+
 ;; Package-Requires: ((emacs "25.1") (compat "28.1.1.0"))
 
-;; This file is not part of GNU Emacs.
+;; SPDX-License-Identifier: GPL-3.0-or-later
 
-;; This file is free software; you can redistribute it and/or modify
-;; it under the terms of the GNU General Public License as published by
-;; the Free Software Foundation; either version 3, or (at your option)
-;; any later version.
-
+;; This file is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published
+;; by the Free Software Foundation, either version 3 of the License,
+;; or (at your option) any later version.
+;;
 ;; This file is distributed in the hope that it will be useful,
 ;; but WITHOUT ANY WARRANTY; without even the implied warranty of
 ;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 ;; GNU General Public License for more details.
-
-;; For a full copy of the GNU General Public License
-;; see <http://www.gnu.org/licenses/>.
-
-;; SPDX-License-Identifier: GPL-3.0-or-later
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with this file.  If not, see <https://www.gnu.org/licenses/>.
 
 ;;; Commentary:
 
